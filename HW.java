77c2cf7d6939f3cdccc566abff4827d7ed81e0c0
DamirCetkovic <damir.cetkovic@gmail.com>
2025-11-06 13:29:29 +0100

    Hello World

--- HW.java
+++ HW.java
@@ -4,5 +4,7 @@ public class HW {
     public static void main(String[] args) {
         System.out.println("Hello Cat");
         System.out.println("Hello Dog");
+
+        System.out.println("Hello World");
     }
 }
