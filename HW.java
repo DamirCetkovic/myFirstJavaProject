a24a8708159cefcfaa991c4c06e0163e8c06a6fe
DamirCetkovic <damir.cetkovic@gmail.com>
2025-11-06 13:18:12 +0100

    Hello Dog

--- HW.java
+++ HW.java
@@ -2,6 +2,6 @@ package staffansGitHub;
 
 public class HW {
     public static void main(String[] args) {
-        System.out.println("Hello Dog);
+        System.out.println("Hello Dog");
     }
 }
